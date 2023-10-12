01e0e825328bebc3b0dc12c1e1876b546f6b3195
JZ-Su <144632998+JZ-Su@users.noreply.github.com>
2023-10-12 11:36:13 +0100

    Add files via upload

---
+++ Beal Conjecture.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <list>
+#include <memory>
+
+class power
+{
+public:
+	power(int base, int index);
+	~power();
+
+private:
+	int base;
+	int index;
+	int pow;
+};
+
+power::power(int _base,int _index):base(_base), index(_index)
+{
+	pow = _base;
+	for (int i = _index; i > 0; i--)
+	{
+		pow *= pow;
+	}
+}
+
+power::~power()
+{
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
